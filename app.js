ec82f3527a88ef7a7baaf19f05991ed18726a4be
manu468 <manojmuppavarapu@gmail.com>
2020-01-03 12:20:14 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const title = React.createElement(
+//create element accepts 3 arguments that defines the elements
+// we want to create,ususally a string a html element or tag
+'h1',
+{id:'main-title', title:'This is the title'},
+'My First React Element!'
+
+);
+console.log(title);
+// we learned that react does not create real dom nodes.by dom nodes
+// i mean h1,div etc. instead react creates plain js objects that describes
+// dom nodes. how does this object representation of the dom node
+//get rendered and displayed on page ???  
+
+// react dom library included in the proj provides DOM Specific methods
+// we will use reactDOM.render () the most , which renders react elements
+// to the dom.
+
+ReactDOM.render(
+// this is the function that actually does the creating and updating
+// of the DOM .
+// render accepts two elements 
+title,
+document.getElementById('root')
+
+//getElementById('root'); is the container element where our code will be
+//rendered by react
+);
